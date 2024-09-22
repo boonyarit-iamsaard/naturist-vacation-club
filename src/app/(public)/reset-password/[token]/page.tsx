61440d6fda3aeb0db714216e62fefa9293c19811
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-09-22 18:23:08 +0700

    feat(auth): [WIP] reset password

---
+++ src/app/(public)/reset-password/[token]/page.tsx
@@ -0,0 +1,9 @@
+import { ResetPasswordForm } from '~/app/(public)/reset-password/_components/reset-password-form';
+
+export default function ResetPasswordPage() {
+  return (
+    <section className="container py-8">
+      <ResetPasswordForm />
+    </section>
+  );
+}
