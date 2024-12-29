5d9ffa321f5b5712d29c67ea9e3ea13cd5c0ab36
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2024-12-29 14:33:17 +0700

    style: improve nav link styles

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,12 +1,13 @@
-@props(['active'])
+@props(['active' => false])
 
 @php
-    $classes =
-        $active ?? false
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out';
+    $base =
+        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 focus:outline-none transition duration-150 ease-in-out';
+    $classes = $active
+        ? 'border-background dark:border-foreground text-background dark:text-foreground focus:border-background dark:focus:border-foreground'
+        : 'border-transparent text-background/60 dark:text-foreground/60 hover:text-background/80 dark:hover:text-foreground/80 hover:border-background/60 dark:hover:border-foreground/60 focus:text-background/80 dark:focus:text-foreground/80 focus:border-background/80 dark:focus:border-foreground/80';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a {{ $attributes->merge(['class' => "$base $classes"]) }}>
     {{ $slot }}
 </a>
